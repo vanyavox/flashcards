62fb56b27c7063a4174b798d2dfbefed26592fb6
Vanya <geo.vox95@gmail.com>
2022-11-04 17:00:19 +0300

    FSIO

--- view/Results.jsx
+++ view/Results.jsx
@@ -5,6 +5,7 @@ module.exports = function Results({ answers, count, username }) {
   return (
     <div className='container'>
       <h1>{username}, вы ответили верно на {count} вопросов</h1>
+      <div className='card'>
       <ul>
           {answers.map((ans) => 
             <li>
@@ -13,6 +14,7 @@ module.exports = function Results({ answers, count, username }) {
               <div className='text-success'>Правильный ответ: {ans.answer}</div>
             </li>)}
         </ul>
+        </div>
 
         <a href="/">Главная</a>
     </div>
